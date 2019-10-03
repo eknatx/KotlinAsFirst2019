a8045486e2863c8686d795bf2c68186217ec69c5
alekseivoroshilov <nekobeardetect@mail.ru>
2019-10-03 14:41:50 +0300

    2-ой урок сделал

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -3,7 +3,11 @@
 package lesson2.task1
 
 import lesson1.task1.discriminant
+import lesson1.task1.sqr
+import javax.tools.Diagnostic
+import kotlin.math.abs
 import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.sqrt
 
 /**
@@ -63,20 +67,18 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
-
-/**
- * Простая
- *
- * Путник двигался t1 часов со скоростью v1 км/час, затем t2 часов — со скоростью v2 км/час
- * и t3 часов — со скоростью v3 км/час.
- * Определить, за какое время он одолел первую половину пути?
- */
-fun timeForHalfWay(
-    t1: Double, v1: Double,
-    t2: Double, v2: Double,
-    t3: Double, v3: Double
-): Double = TODO()
+fun ageDescription(age: Int): String {
+    var answ: String
+    answ = ""
+    if (age % 100 in 5..20) {
+        answ = "лет"
+    } else {
+        if (age % 10 == 1) answ = "год"
+        else if (age % 10 in 2..4) answ = "года"
+        else if (age % 10 in 5..9 || age % 10 == 0) answ = "лет"
+    }
+    return "$age $answ"
+}
 
 /**
  * Простая
@@ -87,44 +89,11 @@ fun timeForHalfWay(
  * и 3, если угроза от обеих ладей.
  * Считать, что ладьи не могут загораживать друг друга
  */
-fun whichRookThreatens(
-    kingX: Int, kingY: Int,
-    rookX1: Int, rookY1: Int,
-    rookX2: Int, rookY2: Int
-): Int = TODO()
-
-/**
- * Простая
- *
- * На шахматной доске стоят черный король и белые ладья и слон
- * (ладья бьет по горизонтали и вертикали, слон — по диагоналям).
- * Проверить, есть ли угроза королю и если есть, то от кого именно.
- * Вернуть 0, если угрозы нет, 1, если угроза только от ладьи, 2, если только от слона,
- * и 3, если угроза есть и от ладьи и от слона.
- * Считать, что ладья и слон не могут загораживать друг друга.
- */
-fun rookOrBishopThreatens(
-    kingX: Int, kingY: Int,
-    rookX: Int, rookY: Int,
-    bishopX: Int, bishopY: Int
-): Int = TODO()
-
-/**
- * Простая
- *
- * Треугольник задан длинами своих сторон a, b, c.
- * Проверить, является ли данный треугольник остроугольным (вернуть 0),
- * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
- * Если такой треугольник не существует, вернуть -1.
- */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
-
-/**
- * Средняя
- *
- * Даны четыре точки на одной прямой: A, B, C и D.
- * Координаты точек a, b, c, d соответственно, b >= a, d >= c.
- * Найти длину пересечения отрезков AB и CD.
- * Если пересечения нет, вернуть -1.
- */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+fun whichRookThreatens(kingX: Int, kingY: Int, rookX1: Int, rookY1: Int, rookX2: Int, rookY2: Int): Int {
+    return when {
+        ((kingX - rookX1 == 0 || kingY - rookY1 == 0) && (kingX - rookX2 == 0 || kingY - rookY2 == 0)) -> 3
+        ((kingX - rookX1 != 0 || kingY - rookY1 != 0) && (kingX - rookX2 == 0 || kingY - rookY2 == 0)) -> 2
+        ((kingX - rookX1 == 0 || kingY - rookY1 == 0) && (kingX - rookX2 != 0 || kingY - rookY2 != 0)) -> 1
+        else -> 0
+    }
+}
