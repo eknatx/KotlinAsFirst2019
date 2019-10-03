d9d57b05146f9fafb83f076e82e6c1b1e167f223
alekseivoroshilov <nekobeardetect@mail.ru>
2019-10-03 14:25:40 +0300

    первый урок сделал

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -19,6 +19,7 @@ fun sqr(x: Int) = x * x
 fun sqr(x: Double) = x * x
 
 /**
+ *
  * Пример
  *
  * Вычисление дискриминанта квадратного уравнения
@@ -59,7 +60,11 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, sec: Int): Int {
+    val respublick = hours * 3600
+    val fuckted  = minutes * 60
+    return respublick + fuckted + sec
+}
 
 /**
  * Тривиальная
@@ -68,7 +73,9 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+    (sagenes*48*4.445+arshins*16*4.445+vershoks*4.445)/100
+
 
 /**
  * Тривиальная
@@ -76,7 +83,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(underWoman: Int, min: Int, sec: Int):Double=( underWoman + min / 60.0 + sec / 3600.0 ) / (180/PI)
+
 
 /**
  * Тривиальная
@@ -84,7 +92,13 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
+    val x = abs (x2 -x1)
+    val y = abs (y2- y1)
+    val s = x * x + y * y
+    return sqrt(s)
+}
 
 /**
  * Простая
@@ -92,7 +106,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = (number /100)% 10
 
 /**
  * Простая
@@ -101,7 +115,8 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+    (hoursArrive*60+minutesArrive)-(hoursDepart*60+minutesDepart)
 
 /**
  * Простая
@@ -110,7 +125,10 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double {
+    val percents = percent / 100.0 + 1.0
+    return (initial * percents * percents * percents)
+}
 
 /**
  * Простая
@@ -118,4 +136,9 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int {
+    val z = number /100
+    val xx = (number% 100) % 10
+    val yyy = (number/ 10) % 10
+    return xx * 100+yyy * 10 + z
+}
